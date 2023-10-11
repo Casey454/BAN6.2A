40a3e5f881c14f9debab08d2bc385e8f602f107d
Casey Portelli <casey.portelli.h64005@mcast.edu.mt>
2023-10-11 11:03:36 +0200

    Architecture+models

---
+++ BAN6.2A/PresentationWebApp1/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationWebApp1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated == false)
+            {
+                //give an error to the user
+                //block the user
+
+
+                TempData["error"] = "Access Denied";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ BAN6.2A/PresentationWebApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PresentationWebApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
